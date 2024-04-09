0fa368fa162bdb0939b1f968143f0e0698c1f164
LinHanLi <h1nlee@163.com>
2024-04-09 20:57:16 +0800

    my_pd
    更新my_pd

--- FreeBack/my_pd.py
+++ FreeBack/my_pd.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from numpy_ext import rolling_apply
+from joblib import Parallel, delayed
 import copy, math
 
 # 关于pandas经常使用到的一些操作 以及对现有函数的改进
@@ -112,9 +113,25 @@ def rolling_reg(df, x_name, y_name, n):
     result = pd.DataFrame(result, index=df.index)
     return result
 
-# 并行计算   可以保留顺序
+def rolling_corr(df, x_name, y_name, n):
+    # 如果这一字段的df长度小于n，直接返回nan,对应index
+    if df.shape[0]<n:
+        result_nan = np.ones(df.shape[0])*np.nan
+        result = pd.Series(result_nan, df.index)
+        return result
+    # 相关性的x和y
+    x = df[x_name]
+    y = df[y_name]
+    def func(x, y):
+        r = x.corr(y)
+        return r 
+    result = rolling_apply(func, n, x, y)
+# 添加index
+    result = pd.DataFrame(result, index=df.index)
+    return result
+
+# 并行计算df, func为对df/ser的操作,返回df/ser 可以保留顺序
 def parallel(df, func, n_core=12):
-    from joblib import Parallel, delayed
     len_df = len(df)
     sp = list(range(len_df)[::int(len_df/n_core+0.5)])[:-1] # 最后一个节点改为末尾
     sp.append(len_df)
@@ -122,86 +139,98 @@ def parallel(df, func, n_core=12):
     results = Parallel(n_jobs=n_core)(delayed(func)(df[slc]) for slc in slc_gen)
     return pd.concat(results)
 
+# 并行group
 def parallel_group(df, func, n_core=12, sort_by='code'):
-    from joblib import Parallel, delayed
     results = Parallel(n_jobs=n_core)(delayed(func)(group) for name, group in df.groupby(sort_by))
     return pd.concat(results)
-
-
 
 # 行情数据的常用计算
 # 数据格式为 mutiindex (date,code)   close...
 
+# 输入series返回series
+# 有并行选项的函数默认并行
+# a 函数可选参数
+def cal_ts(ser, func_name='Max', period=20, a=1, parallel=True, n_core=12):
+    if func_name=='MA':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).mean()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Max':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).max()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Min':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).min()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Std':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).std()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Skew':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).skew()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Kurt':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).kurt()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='Sum':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).sum()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name=='rank':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).rank()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0] 
+    elif func_name=='quantile':
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).quantile(a)).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0] 
+    elif func_name=='Zscore':
+        MA = ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).mean()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+        Std = ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).std()).reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+        return ((ser-MA)/Std).fillna(0)
+    elif func_name=='HV':
+        returns = np.log(ser/ser.groupby(level='code').shift()).fillna(0)
+        return np.exp(returns.groupby(level='code').apply(lambda x: x.droplevel('code').rolling(\
+            period, min_periods=1).std()*np.sqrt(250)).reset_index().sort_values(\
+                by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0])-1
+    elif func_name in ['WMA']:
+        if parallel:
+            def func(ser):
+                return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+                    period, min_periods=1).apply(lambda x: x[::-1].cumsum().sum()/(period*(1+period)/2)))
+            result = parallel_group(ser, func, n_core=n_core)
+            return result.reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).apply(lambda x: x[::-1].cumsum().sum()/(period*(1+period)/2))).reset_index().sort_values(by='date').set_index(\
+                ['date', 'code']).loc[ser.index].iloc[:,0]
+    elif func_name in ['argmin', 'argmax', 'prod']:
+        if parallel:
+            def func(ser):
+                return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+                    period, min_periods=1).apply(getattr(np, func_name)))
+            result = parallel_group(ser, func, n_core=n_core)
+            return result.reset_index().sort_values(by='date').set_index(['date', 'code']).loc[ser.index].iloc[:,0]
+        return ser.groupby(level='code').apply(lambda x: x.droplevel( 'code').rolling(\
+            period, min_periods=1).apply(getattr(np, func_name))).reset_index().sort_values(by='date').set_index(\
+                ['date', 'code']).loc[ser.index].iloc[:,0]
 
-
-# ta-lib
-# SMA 简单移动平均
-# EMA 指数移动平均
-# WMA 加权移动平均
-# DEMA 双指数移动平均
-# TEMA 三重指数移动平均
-
-
-# 计算时间序列值
-# 数据df，函数名(Max, Min, Skew, Kurt, MA, Std)
-# 作用字段，滚动时间窗口长度，是否并行，并行核数
-# rolling输入series而不是df速度会明显提升
-def cal_TS(df, func_name='Max', cal='close', period=20, parallel=False, n_core=12):
-    df = copy.deepcopy(df)
-# inde必须为 'code'和'date'，并且code内部的date排序
-    df = df.reset_index().sort_values(by='code').set_index(['code', 'date']).sort_index(level=['code', 'date'])
-    new_col = cal + '_' + func_name + '_' + str(period)
-    if parallel:
-        def func(df):
-            if func_name=='Max':
-                return df[cal].rolling(period, min_periods=1).max()
-            elif func_name=='Min':
-                return df[cal].rolling(period, min_periods=1).min()
-            elif func_name=='Skew':
-                return df[cal].rolling(period, min_periods=1).skew()
-            elif func_name=='Kurt':
-                return df[cal].rolling(period, min_periods=1).kurt()
-            elif func_name=='MA':
-                return df[cal].rolling(period, min_periods=1).mean()
-            elif func_name=='Std':
-                return df[cal].rolling(period, min_periods=1).std()
-            elif func_name=='Sum':
-                return df[cal].rolling(period, min_periods=1).sum()
-            elif func_name=='Zscore':
-                return (df[cal]-df[cal].rolling(period, min_periods=1).mean())/df[cal].rolling(period, min_periods=1).std().replace(0, np.nan)
-            elif func_name=='HV':
-                returns = (df[cal]/(df[cal].shift())).apply(lambda x: np.log(x))
-                return np.exp(returns.rolling(period, min_periods=1).std() * np.sqrt(252))-1
-        df[new_col] =  parallel_group(df, func, n_core=n_core).values
-    else:
-        if func_name=='Max':
-            df[new_col] =  df.groupby('code', sort=False)[cal].rolling(period, min_periods=1).max().values
-        elif func_name=='Min':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].min().values
-        elif func_name=='Skew':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].skew().values
-        elif func_name=='Kurt':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].kurt().values
-        elif func_name=='MA':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].mean().values
-        elif func_name=='Std':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].std().values
-        elif func_name=='Sum':
-            df[new_col] =  df.groupby('code', sort=False).rolling(period, min_periods=1)[cal].sum().values
-        elif func_name=='Zscore':
-            df[new_col] =  (df[cal].values - df.groupby('code', sort=False)[cal].rolling(period, min_periods=1).mean().values)\
-                             /df.groupby('code', sort=False)[cal].rolling(period, min_periods=1).std().replace(0, np.nan).values
-            df[new_col] = df[new_col].fillna(0)
-        elif func_name=='HV':
-            df['returns'] = (df[cal]/(df[cal].shift())).apply(lambda x: np.log(x))
-            df[new_col] =  np.exp(df.groupby('code', sort=False)['returns'].rolling(period, min_periods=1).std().values * np.sqrt(252))-1
-
-# 将index变回 date code
-    df = df.reset_index().sort_values(by='date').set_index(['date', 'code'])
-    return df
-
-# 获得df中x_name列为自变量 y_name列为因变量的线性回归结果 
-def cal_reg(df, x_name, y_name, n, parallel=False, n_core=12):
+# 按照look列的阈值筛选得到cal列，求和
+def cal_select_sum(df, key, threshold_left, threshold_right, sum, n, n_core=12):
+    # 按条件筛选滚动求和
+    def rolling_select_sum(df, key, threshold_left, threshold_right, sum, n):
+        # 如果这一字段的df长度小于n，直接返回nan,对应index
+        if df.shape[0]<n:
+            result_nan = np.ones(df.shape[0])*np.nan
+            result = pd.Series(result_nan, df.index)
+            return result
+        # 相关性的x和y
+        x = df[key]
+        tl = df[threshold_left]
+        tr = df[threshold_right]
+        y = df[sum]
+        def func(x, tl, tr, y):
+            return ((x>=tl.iloc[-1])*(x<=tr.iloc[-1])*y).sum()
+        result = rolling_apply(func, n, x, tl, tr, y)
+# 添加  index
+        result = pd.DataFrame(result, index=df.index)
+        return result
+    # 按代码并行
     df = copy.deepcopy(df)
     # inde必须为 'code'和'date'，并且code内部的date排序
     df = df.reset_index()
@@ -209,9 +238,30 @@ def cal_reg(df, x_name, y_name, n, parallel=False, n_core=12):
     df = df.set_index(['code','date'])
     df = df.sort_index(level=['code','date'])
     # 命名规则
-    name_beta = x_name + '-' + y_name + '--beta' +str(n)
-    name_alpha = x_name + '-' + y_name + '--alpha'+str(n)
-    name_r = x_name + '-' + y_name + '--r'+str(n)
+    name_r = str(sum) + '_' + 'Sum' + str(n) + '-' + str(key) + ',' + str(threshold_left) + ';' + str(threshold_right)
+    def func(df):
+        return rolling_select_sum(df.reset_index('code'), key, threshold_left, threshold_right, sum, n)
+    df[[name_r]] =  parallel_group(df, func, n_core=n_core).values
+    # 将index变回 date code
+    df = df.reset_index()
+    df = df.sort_values(by='date')
+    df = df.set_index(['date','code'])
+    df = df.sort_index(level=['date','code'])
+    return df[name_r]
+
+
+# 获得df中x_name列为自变量 y_name列为因变量的线性回归结果 
+def cal_reg(df, x_name, y_name, n, parallel=True, n_core=12):
+    df = copy.deepcopy(df)
+    # inde必须为 'code'和'date'，并且code内部的date排序
+    df = df.reset_index()
+    df = df.sort_values(by='code')
+    df = df.set_index(['code','date'])
+    df = df.sort_index(level=['code','date'])
+    # 命名规则
+    name_beta = str(x_name) + '-' + str(y_name) + '--beta' +str(n)
+    name_alpha = str(x_name) + '-' + str(y_name) + '--alpha'+str(n)
+    name_r = str(x_name) + '-' + str(y_name) + '--r'+str(n)
     if parallel:
         def func(df):
             return rolling_reg(df.reset_index('code'), x_name, y_name, n)
@@ -225,18 +275,65 @@ def cal_reg(df, x_name, y_name, n, parallel=False, n_core=12):
     df = df.sort_values(by='date')
     df = df.set_index(['date','code'])
     df = df.sort_index(level=['date','code']) 
-
     return df
 
-def cal_CrossReg(df_, x_name, y_name, series=False):
-    df = copy.copy(df_)
-    name = y_name + '-' + x_name + '--alpha'
-    beta = df.groupby('date').apply(lambda x: ((x[y_name]-x[y_name].mean())*(x[x_name]-x[x_name].mean())).sum()/((x[x_name]-x[x_name].mean())**2).sum())
-    gamma = df.groupby('date').apply(lambda x: x[y_name].mean() - beta[x.index[0][0]]*x[x_name].mean())
-    r = df.groupby('date').apply(lambda x: np.sqrt(((gamma[x.index[0][0]]+x[x_name]*beta[x.index[0][0]] - x[y_name].mean())**2).sum()/(((gamma[x.index[0][0]]+x[x_name]*beta[x.index[0][0]] - x[y_name].mean())**2).sum() + ((x[y_name]-(gamma[x.index[0][0]] + x[x_name]*beta[x.index[0][0]]))**2).sum()))) 
-
-    if series:
-        return df, beta, gamma, r
+def cal_corr(df, x_name, y_name, n, parallel=True, n_core=12):
+    df = copy.deepcopy(df)
+    # inde必须为 'code'和'date'，并且code内部的date排序
+    df = df.reset_index()
+    df = df.sort_values(by='code')
+    df = df.set_index(['code','date'])
+    df = df.sort_index(level=['code','date'])
+    # 命名规则
+    name_r = str(x_name) + '-' + str(y_name) + '--r'+str(n)
+    if parallel:
+        def func(df):
+            return rolling_corr(df.reset_index('code'), x_name, y_name, n)
+        df[[name_r]] =  parallel_group(df, func, n_core=n_core).values
     else:
-        df[name] = df.groupby('date').apply(lambda x: x[y_name] - beta[x.index[0][0]]*x[x_name] - gamma[x.index[0][0]]).values
-        return df
+        # 回归    去掉二级index中的code
+        df_reg = df.groupby('code', sort=False).apply(lambda df: \
+                        rolling_corr(df.reset_index('code'), x_name, y_name, n))
+        df[[name_r]] = df_reg
+    # 将index变回 date code
+    df = df.reset_index()
+    df = df.sort_values(by='date')
+    df = df.set_index(['date','code'])
+    df = df.sort_index(level=['date','code']) 
+    return df
+
+
+# x_name list, y_nmae column
+# 截面多元线性回归
+def cal_CrossReg(df, x_name, y_name, residual=False):
+    import statsmodels.api as sm
+    # 使用sm模块
+    result = df.groupby('date', sort=False).apply(lambda d:\
+                 sm.OLS(d[y_name], sm.add_constant(d[x_name])).fit())
+    
+    # 如果d[x_name]中所有数相同为C且不为零，这时params中没有const，x_name为d[y_name].mean()/C
+    # rsquared为0
+    # 当d[x_name]全为0时，params['const']为0，params[x_name]为d[y_name].mean()
+    # rsquared可能为极小的负数
+    def func(x, name):
+        try:
+            return x.params[name]
+        except:
+            print('sm reg warning')
+            return 0
+    #if type(x_name)!=type([]):
+    r = result.map(lambda x: np.sqrt(abs(x.rsquared)))
+
+    data = []
+    index = []
+    for i in result.items():
+        data.append(dict(i[1].params))
+        index.append(i[0])
+    params = pd.DataFrame(data, index=index)
+
+    if residual:
+        return pd.Series(df.groupby('date').apply(lambda x: x[y_name] - \
+                    (params.loc[x.index[0][0]][x_name]*x[x_name]).sum(axis=1) -\
+                        params.loc[x.index[0][0]]['const']).values, index=df.index)
+    else:
+        return params, r
