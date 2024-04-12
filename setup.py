52a9cfdf9de67fde2ce4dca1cad67b3cf42f4ba7
LinHanLi <h1nlee@163.com>
2024-04-12 09:42:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
 
-with open("README.md","r") as fh:
+with open("README.md","r",encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
