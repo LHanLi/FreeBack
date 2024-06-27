50be18236c1cbe84f3c762879389e01a349f7a0b
Q <1321529490@qq.com>
2024-06-27 15:53:58 +0800

    post修改bug

--- FreeBack/post.py
+++ FreeBack/post.py
@@ -27,7 +27,7 @@ def check_output():
 class ReturnsPost():
     # returns,简单收益率序列  type:pd.Series index:pd.DatetimeIndex 
     # benchmark,基准收益率序列（可以多个） pd.DataFrame index:pd.DatetimeIndex, 0表示不设基准 
-    def __init__(self, returns, benchmark=0, stratname='策略', freq='day', rf=0.03, fast=False):
+    def __init__(self, returns, benchmark=None, stratname='策略', freq='day', rf=0.03, fast=False):
         self.stratname = stratname
         self.returns = returns.fillna(0)
         # returns频率， 目前支持day, week
@@ -54,17 +54,17 @@ class ReturnsPost():
             a = np.maximum.accumulate(self.net)
             self.drawdown = (a-self.net)/a
             # 基准指数
-            if benchmark==0:
+            if type(benchmark) == type(None):
                 benchmark = pd.DataFrame(index = self.returns.index)
                 benchmark['zero'] = 0
                 self.benchmark = benchmark
             self.benchmark = benchmark.loc[self.returns.index].fillna(0)
         else: 
             # 基准指数
-            if benchmark==0:
+            if type(benchmark) == type(None):
                 benchmark = pd.DataFrame(index = self.returns.index)
                 benchmark['zero'] = 0
-                self.benchmark = benchmark
+            self.benchmark = benchmark
             self.sigma_benchmark = np.exp(np.log(self.benchmark[\
                 self.benchmark.columns[0]]+1).std())-1
             self.cal_detail()
