6df1832306091bf82ee7c1f3fe9225ba00a847da
LinHanLi <h1nlee@163.com>
2024-08-21 10:10:09 +0800

    Update __init__.py

--- FreeBack/__init__.py
+++ FreeBack/__init__.py
@@ -1,2 +1,2 @@
 # __init__.py
-from FreeBack import signal, strat, alpha, barbybar,display,event,post,opt,my_pd 
+from FreeBack import signal, strat, alpha, barbybar,display,event,post,opt,my_pd,debug 
